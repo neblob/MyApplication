6c8f62ad97c449b20eb9c36360c5dc9dd55d50db
neblob <neblob_@msn.com>
2015-07-08 13:06:24 +0700

    กำหนดทิศทางกล้องให้สอดคล้อง activity

--- app/src/main/java/com/example/toshiba/myapplication/MySurfaceView.java
+++ app/src/main/java/com/example/toshiba/myapplication/MySurfaceView.java
@@ -5,8 +5,10 @@ import java.util.List;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.AttributeSet;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 public class MySurfaceView extends SurfaceView
                             implements SurfaceHolder.Callback   {
@@ -57,6 +59,37 @@ public class MySurfaceView extends SurfaceView
         mCamera.stopPreview();
         mCamera.release();
     }
+    public void setmCameraDisplayOrientation() {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(0, info);
 
+        int rotation = (WindowManager) mContext
+                .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                .getRotation();
+    }
+        int degrees = 0;
+        switch (rotation) {
+        case Surface.ROTATION_0:
+            degrees = 0 ;
+            break;
+        case Surface.ROTATION_90:
+            degrees = 90 ;
+            break;
+        case Surface.ROTATION_180:
+            degrees = 180 ;
+            break;
+        case Surface.ROTATION_270:
+            degrees = 270 ;
+            break;
+    }
 
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT)
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;
+        } else{
+            result=(info.orientation-degrees+360)%360;
+        }
+        mCamera.setDisplayOrientation(result);
 }
