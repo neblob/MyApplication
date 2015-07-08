a7179f7f6ba8269b1f83862c2cd2848073662f12
neblob <neblob_@msn.com>
2015-07-08 15:24:33 +0700

    Code App + เขียนเส้นถ่ายภาพได้ + เซฟได้ตัวเต็ม

--- app/src/main/java/com/example/toshiba/myapplication/MainActivity.java
+++ app/src/main/java/com/example/toshiba/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -14,22 +15,31 @@ public class MainActivity extends Activity {
     private static final int TAKE_PICTURE = 100;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button shutterButton = (Button) findViewById(R.id.shutter_button);
+        shutterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySurfaceView.takePicture();
 
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
-            ImageView image = (ImageView) findViewById(R.id.imageView);
+        ImageView image = (ImageView) findViewById(R.id.imageView);
 
-            if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK ) {
+        if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK) {
             Bitmap captureImage = (Bitmap)
-                                    data.getExtras().get("data");
+                    data.getExtras().get("data");
             image.setImageBitmap(captureImage);
-            }
+        }
+
     }
+
 }
--- app/src/main/java/com/example/toshiba/myapplication/MySurfaceView.java
+++ app/src/main/java/com/example/toshiba/myapplication/MySurfaceView.java
@@ -1,14 +1,20 @@
 package com.example.toshiba.myapplication;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MySurfaceView extends SurfaceView
         implements SurfaceHolder.Callback {
@@ -98,5 +104,38 @@ public class MySurfaceView extends SurfaceView
             result = (info.orientation - degrees + 360) % 360;
         }
         mCamera.setDisplayOrientation(result);
+    }
+
+    private static class TakePictureCallback
+            implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            try {
+                File imageFile = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+                FileOutputStream fos = new FileOutputStream(imageFile);
+                fos.write(data);
+                fos.flush();
+                fos.close();
+
+                Toast.makeText(mContext, "Picture saved.",
+                        Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public static void takePicture() {
+        Camera.Parameters params = mCamera.getParameters();
+        List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+        Camera.Size selected = sizes.get(0);
+        params.setPictureSize(selected.width, selected.height);
+        mCamera.setParameters(params);
+
+        TakePictureCallback callback = new TakePictureCallback();
+        mCamera.takePicture(null, null, null, callback);
     }
 }
