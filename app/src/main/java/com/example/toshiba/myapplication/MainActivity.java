f7c8f7c6e09db420c3862943c6d2b7cb1fa44519
neblob <neblob_@msn.com>
2015-07-08 11:41:56 +0700

    Basic code camera app

--- app/src/main/java/com/example/toshiba/myapplication/MainActivity.java
+++ app/src/main/java/com/example/toshiba/myapplication/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
-            ImageView image = (ImageView) findViewById(R.id.image);
+            ImageView image = (ImageView) findViewById(R.id.imageView);
 
             if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK ) {
             Bitmap captureImage = (Bitmap)
--- app/src/main/java/com/example/toshiba/myapplication/MySurfaceView.java
+++ app/src/main/java/com/example/toshiba/myapplication/MySurfaceView.java
@@ -1,24 +1,24 @@
 package com.example.toshiba.myapplication;
 
-import java.util.List;
-
-import android.content.Context;
-import android.hardware.Camera;
-import android.util.AttributeSet;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-public class MySurfaceView extends SurfaceView
-                            implements SurfaceHolder.Callback   {
-    private static Context mContext;
-    private static Camera mCamera;
-
-    public MySurfaceView(Context context) {
-        this(context, null);
-    }
-
-    public MySurfaceView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-    @SuppressWarnings(deprecation)
-}
+//import java.util.List;
+//
+//import android.content.Context;
+//import android.hardware.Camera;
+//import android.util.AttributeSet;
+//import android.view.SurfaceHolder;
+//import android.view.SurfaceView;
+//
+//public class MySurfaceView extends SurfaceView
+//                            implements SurfaceHolder.Callback   {
+//    private static Context mContext;
+//    private static Camera mCamera;
+//
+//    public MySurfaceView(Context context) {
+//        this(context, null);
+//    }
+//
+//    public MySurfaceView(Context context, AttributeSet attrs) {
+//        this(context, attrs, 0);
+//    }
+//    @SuppressWarnings(deprecation)
+//}
